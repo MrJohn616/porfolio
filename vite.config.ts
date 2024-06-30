8c2ac0ac550bc2b75759364e63f58834a8fb86e6
MrJohn616 <a351giova1nni385.francisco10@gmail.com>
2024-06-30 08:54:56 -0600

    Update bttns

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://MrJohn616.github.io/porfolio--giovanni-francisco-gonzalez-ruiz",
+  base: "https://MrJohn616.github.io/s
 });
